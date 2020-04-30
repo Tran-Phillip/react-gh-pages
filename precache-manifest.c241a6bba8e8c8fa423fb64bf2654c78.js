4d351d326b9d2500bccc5f141b665acf95aaff79
Phillip Tran <pntran@ucdavis.edu>
2020-04-30 15:46:27 -0700

    Updates

---
+++ precache-manifest.c241a6bba8e8c8fa423fb64bf2654c78.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2026b930e1821c5037caf26b62344ff0",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "933095e7424f2dcf9680",
+    "url": "/react-gh-pages/static/css/main.3b6ac00f.chunk.css"
+  },
+  {
+    "revision": "cfa0b180c93b56983b30",
+    "url": "/react-gh-pages/static/js/2.fe73393b.chunk.js"
+  },
+  {
+    "revision": "ee0b6c573819ac3bd62d7bd7f901db2d",
+    "url": "/react-gh-pages/static/js/2.fe73393b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933095e7424f2dcf9680",
+    "url": "/react-gh-pages/static/js/main.89f4186e.chunk.js"
+  },
+  {
+    "revision": "b15fab85dea6ea389910",
+    "url": "/react-gh-pages/static/js/runtime-main.b1e97c2c.js"
+  },
+  {
+    "revision": "effd317999c990ab86f550886b80b254",
+    "url": "/react-gh-pages/static/media/ConnectButton.effd3179.png"
+  },
+  {
+    "revision": "99a95dd4e901a6b1870b276106beada0",
+    "url": "/react-gh-pages/static/media/InnerCircle.99a95dd4.png"
+  },
+  {
+    "revision": "d9251c48ad34be6e57580715ba7f0bdf",
+    "url": "/react-gh-pages/static/media/MiddleButtons.d9251c48.png"
+  },
+  {
+    "revision": "2fc3c0ee9cd10f1fe7a4b38de48fcb33",
+    "url": "/react-gh-pages/static/media/MovementButton.2fc3c0ee.png"
+  },
+  {
+    "revision": "5045274a4f32df72a5d8f948a8b4c80e",
+    "url": "/react-gh-pages/static/media/MovementButtonOuterRing.5045274a.png"
+  },
+  {
+    "revision": "4c90771b816ffe275626e4f7666e6a9e",
+    "url": "/react-gh-pages/static/media/OuterRing.4c90771b.png"
+  },
+  {
+    "revision": "ce3e3fd6e629502f0c5e904d26516cc5",
+    "url": "/react-gh-pages/static/media/RotateButton.ce3e3fd6.png"
+  },
+  {
+    "revision": "0ba72823d516525e824f5894d69cd13a",
+    "url": "/react-gh-pages/static/media/UploadButton.0ba72823.png"
+  }
+]);
